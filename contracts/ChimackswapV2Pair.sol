f77a624d7ceb0ccf10cd62040f0d36c2dcd3d0d1
JJakDDo <kangdw124@gmail.com>
2022-05-04 14:23:16 +0900

    Feat: initial setting

---
+++ contracts/ChimackswapV2Pair.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5;
+
+contract ChimackswapV2Pair {
+  address public token0;
+  address public token1;
+  uint256 private reserve0;
+  uint256 private reserve1;
+
+  function mint() public {
+
+  }
+}
